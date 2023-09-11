7ba7cca39b44b8f8a11898ded4d04013f90312b4
groot85 <68751054+groot85@users.noreply.github.com>
2023-09-11 15:00:21 +0000

    exercicio versao final

--- main.py
+++ main.py
@@ -16,8 +16,7 @@
 #"encapsulamento"
 #"classe abstrata"
      
-#obs.: classe abstrata possui metodos abstratos, não tem implementacao. Funciona como se fosse um "template" para a classe filha).
-#cliente homem e cliente mulher serão herdeiros de Clientes
+#obs.: classe abstrata possui metodos abstratos, não tem implementacao. Funciona como se fosse um "template" para a classe filha) | #cliente homem e cliente mulher serão herdeiros de Clientes
 #@property #a propriedade serve para acessar pelo mundo externo a variável que está escondida, que so tem no mundo interno ex:  _variavel ex _nome
 
 from abc import ABC, abstractmethod
